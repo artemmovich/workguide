9bfc6cfc461454290791c402b739480728ccb0fc
Artem <strikerlutsk@gmai.comuser.email>
2024-01-16 14:04:01 +0100

    add JS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
+
+const menuBtn = document.querySelector(".menu-btn")
+const titleBtn = document.querySelector(".intro__title")
+const menuMobile = document.querySelector(".menu-mobile")
+const navBar = document.getElementsByClassName("menu-item")
+
+menuBtn.addEventListener('click', () => {
+   menuMobile.classList.toggle('menu--open'),
+      menuBtn.classList.toggle('active');
+})
+titleBtn.addEventListener('click', () => {
+   menuMobile.classList.toggle('menu--open');
+   menuBtn.classList.toggle('active');
+});
+function handleNavBarClick() {
+   menuMobile.classList.toggle("menu--open"),
+   menuBtn.classList.toggle('active');
+}
+for (let i = 0; i < navBar.length; i++) {
+   navBar[i].addEventListener("click", handleNavBarClick);
+ }
+
+
 jQuery(document).ready(function ($) {
    let header = $("#header");
    let intro = $("#intro");
    let nav = $("#nav");
-   
+
    let introH = intro.innerHeight();
-   let scrollPos = $(window).scrollTop();
-   checkScroll(scrollPos, introH);
+   let scroll = $(window).scrollTop();
+   checkScroll(scroll, introH);
+   
 
 
    $(window).on("scroll  resize", function () {
@@ -15,35 +39,22 @@ jQuery(document).ready(function ($) {
       checkScroll(scrollPos, introH)
    });
    function checkScroll(scrollPos, introH) {
-      if (scrollPos > introH) {
+      if (scrollPos > 544) {
          header.addClass("fixed");
       } else {
          header.removeClass("fixed");
       }
    };
-   $("[data-scroll]").on("click", function (event) {
+   $("a.menu__link").on("click", function (event) {
+      $("html, body").animate({
+         scrollTop: elementOffset - 100
+      }, 800);
       event.preventDefault();
 
       let elementId = $(this).data('scroll');
       let elementOffset = $(elementId).offset().top;
-      nav.removeClass("show");
-
-      $("html, body").animate({
-         scrollTop: elementOffset - 70
-      }, 700);
+      nav.removeClass("menu--open");
    });
 
 })
-const menuBtn = document.querySelector(".menu-btn")
-const titleBtn = document.querySelector(".intro__title")
-const menuMobile = document.querySelector(".menu-mobile")
-menuBtn.addEventListener('click', () => {
-   menuMobile.classList.toggle('menu--open'),
-   menuBtn.classList.toggle('active');
-})
-titleBtn.addEventListener('click', () => {
-   menuMobile.classList.toggle('menu--open');
-   menuBtn.classList.toggle('active');
-});
-
 
